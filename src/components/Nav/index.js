cca62abaf9fbbd7117dc5a36539401d29a73ab24
crosgar <cecilia.rossi.garcia@gmail.com>
2021-08-27 13:44:28 -0700

    navbar switch to react bootstrap navbar, buggy

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,28 +1,18 @@
 import React from "react";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import './Nav.css';
 
 function Nav({ pages = [], setCurrentPage, currentPage }) {
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container-fluid">
-        <h1 className="navbar-brand mb-0 h1">
-          <a href="/">Cecilia Rossi-Garc&#237;a</a>
-        </h1>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">Cecilia Rossi-Garc&#237;a</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
             {pages.map((page) => (
-              <li className="nav-item" key={page.id}>
+              <Nav.Item key={page.id}>
                 <span
                   className={currentPage.name === page.name ? 'nav-link current-page' : 'nav-link'}
                   onClick={() => {
@@ -31,12 +21,12 @@ function Nav({ pages = [], setCurrentPage, currentPage }) {
                 >
                   {page.name}
                 </span>
-              </li>
+              </Nav.Item>
             ))}
-          </ul>
-        </div>
-      </div>
-    </nav>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Resume() {
    return (
-      <div>resume goes here</div>
+      <div><a href="https://drive.google.com/file/d/1588OsXSbpIkiUf_Tq7IrQ3rwIgZgVvn6/view?usp=sharing" target="_blank" rel="noreferrer">click here to download my resume</a></div>
    )
 }
 
