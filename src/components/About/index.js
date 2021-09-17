40eba15db43deff578e0b25618cd4a0be9a8704b
crosgar <cecilia.rossi.garcia@gmail.com>
2021-09-17 15:21:30 -0700

    updated about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,18 +6,15 @@ import './About.css'
 function About() {
    return (
      <Row>
-       <Col xs={{ span: 10, offset: 1 }} md={{span: 3, offset: 0}}>
+       <Col xs={{ span: 10, offset: 1 }} md={{ span: 3, offset: 0 }}>
          <img src={self} alt="portrait of me" className="portrait"></img>
        </Col>
-       <Col xs={{ span: 10, offset: 1 }} md={{span: 7, offset: 1}}>
+       <Col xs={{ span: 10, offset: 1 }} md={{ span: 7, offset: 1 }}>
          <p className="about-text">
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-           ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-           aliquip ex ea commodo consequat. Duis aute irure dolor in
-           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-           culpa qui officia deserunt mollit anim id est laborum.
+           Former photographer and store manager turned web developer. Acute
+           sense of responsibility, organized, and attentive to detail.
+           Extensive experience with group work and adapting to different
+           leadership.
          </p>
        </Col>
      </Row>
